64fab9eed5e67e36e2abcc80e1579a9386b3d00b
gilbus <github@tinkershell.eu>
2018-06-13 16:30:31 +0200

    try to fix user data script

--- client_user_data.sh
+++ client_user_data.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install ansible
+sleep 120
+sudo apt-get install software-properties-common --yes
+sudo apt-add-repository ppa:ansible/ansible --yes
+sleep 30
+sudo apt-get update --yes
+while [[ ! -f /var/lib/dpkg/lock ]]; do
+	sleep 2
+done
+sudo apt-get install ansible --yes
+
+wget https://raw.githubusercontent.com/thestinger/termite/master/termite.terminfo
+tic -x termite.terminfo
 
 sudo ansible-pull -o -U https://github.com/deNBI/database_transfer.git -C cvmfs_client_ansible > /dev/null
